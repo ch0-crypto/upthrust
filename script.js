991f668e6661e14328ebb55f4899acabc702c6f9
emeka.chidiebere <carmie_chris@yahoo.com>
2022-03-26 19:21:22 +0100

    eight commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-// document.querySelector(".programs").addEventListener("click", function () {
-//   const nested = (document.querySelector(".packges");
-// });
+const navBar = document.querySelector(".navbar");
+
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    navBar.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
